7aad467903cdf700a3e98a2ba523b559d019a6db
Felipe Álvarez <felipealvarez@letsta.lk>
2021-02-16 10:03:38 -0300

    Ping

--- dj_proposals_candidates/create_proposals_from_data.py
+++ dj_proposals_candidates/create_proposals_from_data.py
@@ -1,8 +1,10 @@
 from dj_proposals_candidates.models import Territory, Proposal, Candidate, Commitment
 import numpy as np
 
+
 def dummy_representation(proposal):
     return np.zeros(1)
+
 
 def process_proposals_from_data(data, url, get_representation_function=dummy_representation):
     for assembly in data['data']['assemblies']:
@@ -13,36 +15,45 @@ def process_proposals_from_data(data, url, get_representation_function=dummy_rep
             territory = Territory.objects.create(remote_id=territory_id, name=territory_name)
         else:
             territory = Territory.objects.get(remote_id=territory_id)
-        proposals_component = next(filter(lambda component: component['__typename'] == 'Proposals', assembly['components']), None)
+        proposals_component = next(
+            filter(lambda component: component['__typename'] == 'Proposals', assembly['components']), None)
         if proposals_component:
             proposals = proposals_component['proposals']['edges']
+            proposals_component_id = proposals_component['id']
             for p_dict in proposals:
                 proposal_dict = p_dict['node']
                 proposal_id = proposal_dict['id']
                 proposal_exists = Proposal.objects.filter(remote_id=proposal_id).exists()
                 if not proposal_exists:
+                    remote_url = '/assemblies/{territory_id}/f/{proposals_component_id}/proposals/{proposal_id}'
+                    remote_url = remote_url.format(territory_id=territory_id,
+                                                   proposals_component_id=proposals_component_id,
+                                                   proposal_id=proposal_id)
+                    remote_url = url + remote_url
                     proposal = Proposal.objects.create(remote_id=proposal_id,
-                                                        title=proposal_dict['title'],
-                                                        territory=territory,
-                                                        description=proposal_dict['body'],
-                                                        votes = proposal_dict['voteCount'])
+                                                       title=proposal_dict['title'],
+                                                       territory=territory,
+                                                       description=proposal_dict['body'],
+                                                       votes=proposal_dict['voteCount'],
+                                                       remote_url=remote_url
+                                                       )
                     proposal.representation = get_representation_function(proposal)
                 else:
                     proposal = Proposal.objects.get(remote_id=proposal_id)
                 for endorsement in p_dict['node']['endorsements']:
-                    profile_path = url+endorsement['profilePath']
+                    profile_path = url + endorsement['profilePath']
                     candidate_exists = Candidate.objects.filter(profile_path=profile_path).exists()
                     if not candidate_exists:
                         candidate = Candidate.objects.create(
                             profile_path=profile_path,
                             name=endorsement['name'],
-                            img_url=url+endorsement['avatarUrl'],
+                            img_url=url + endorsement['avatarUrl'],
                             nickname=endorsement['nickname'],
                             territory=territory
                         )
                     else:
                         candidate = Candidate.objects.get(profile_path=profile_path)
-                    
-                    commitment, created_commitment = Commitment.objects.get_or_create(candidate=candidate, proposal=proposal)
+
+                    commitment, created_commitment = Commitment.objects.get_or_create(candidate=candidate,
+                                                                                      proposal=proposal)
                     print(commitment)
-                      
--- dj_proposals_candidates/models.py
+++ dj_proposals_candidates/models.py
@@ -39,6 +39,7 @@ class Proposal(TimeStampedModel):
     description = models.TextField()
     representation = models.BinaryField(default=None, null=True, blank=True)
     votes = models.IntegerField()
+    remote_url = models.URLField(blank=True)
     
     def __str__(self):
         return self.title
